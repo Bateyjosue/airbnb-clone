ac18206eb24f7b6b91f28a32d16846c9170ebd0a
Bateyjosue <josuebatey19@gmail.com>
2022-09-10 16:52:36 +0200

    add the font to be used and color #2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      
+      <div>
+        Hello Airbnb Lover
+      </div>
     );
   }
 }
