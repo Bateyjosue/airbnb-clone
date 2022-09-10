4af1974596cf42705b43d7efefe41a1b5d6c95fd
Bateyjosue <josuebatey19@gmail.com>
2022-09-10 17:57:41 +0200

    Mount the Header COmponent to the root file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './styles/App.css';
+import Header from './components/header';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello Airbnb Lover
-      </div>
+      <Header />
     );
   }
 }
