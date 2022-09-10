051270bc1fc0e09a2e536fc4fddacaef4d141569
Bateyjosue <josuebatey19@gmail.com>
2022-09-10 22:52:48 +0200

    Solve linters errors

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './styles/App.css';
 import Header from './components/header';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     return (
